1f5ab8b9fc9554078758d077cc55d14bfe03b7be
k3gc <119272540+k3gc@users.noreply.github.com>
2023-02-17 06:55:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ while true; do
 			
 logo "Installing needed packages.."
 
-dependencias=(bspwm polybar sxhkd dunst rofi cmatrix lxappearance pcmanfm nitrogen)
+dependencias=(dunst rofi cmatrix lxappearance pcmanfm nitrogen)
 
 printf "%s%sChecking for required packages...%s\n" "${BLD}" "${CBL}" "${CNC}"
 for paquete in "${dependencias[@]}"
