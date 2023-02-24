484fb3643c0cb94bcf55802ac76c89d980509cfa
k3gc <119272540+k3gc@users.noreply.github.com>
2023-02-23 22:38:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 function install_dotfiles(){
-	CRE=$(tput setaf 1)
-	CYE=$(tput setaf 3)
-	CGR=$(tput setaf 2)
-	CBL=$(tput setaf 4)
-	BLD=$(tput bold)
-	CNC=$(tput sgr0)
-	OK='\n\033[0;32m OK...\033[0m'
-
+	clear
 	miku_logo () {
-	
-		local text="${1:?}"
-		echo -en "⠀
+		echo -en "\e[1;36m
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣶⣶⣶⣶⣶⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⢏⣼⠿⣦⠀⣠⣶⣿⣿⡿⣟⣿⣯⣿⢯⣿⣯⣿⢿⣿⣿⣦⣄⣀⣾⢿⣎⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⢣⡿⣛⣷⣿⣿⣿⢿⣯⣷⣿⡿⣯⣷⣿⢿⣯⣷⡿⣿⣻⣾⢿⡿⣿⣷⣿⣙⣧⡻⣦⠀⠀⠀⠀⢀⠀⠀⠀⠀
@@ -38,47 +29,48 @@ function install_dotfiles(){
 	⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⣿⣻⣷⣯⠛⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠾⠛⣽⣿⣯⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀
 	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢽⣷⣄⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⣠⣾⡿⠗⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 	⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠓⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                             
-                                       Sofy_32 Script\n\n"
-    	printf ' %s [%s%s %s%s %s]%s\n\n' "${CRE}" "${CNC}" "${CYE}" "${text}" "${CNC}" "${CRE}" "${CNC}"
-    	sleep 2
+
+                                            By sofi_32\n\n"
+		sleep 2
 	}
 
-	miku_logo "Welcome"
-	printf '%sThis script will install needed dependencies and copy my dotfiles to your bspwm setup.%s\n'"${CRE}" "${CNC}"
+	miku_logo
+	echo -e "\e[1;32m bienvenido"
+	echo -e "\e[1;32m Este script instalará las dependencias necesarias y copiará mis dotfiles a su configuración de bspwm."
 	sleep 5
 
 	while true; do
-		read -rp " Do you wish to continue? [y/N]: " yn
+		read -rp " Desea continuar? [y/N]: " yn
 			case $yn in
 				[Yy]* ) break;;
 				[Nn]* ) exit;;
-				* ) printf " Error: just write 'y' or 'n'\n\n";;
+				* ) printf " Error: solo escribe 'y' o 'n'\n\n";;
 			esac
     	done
 				clear
-			
-	miku_logo "Installing needed packages.."
 
-	dependencias=(dunst rofi cmatrix polybar lxappearance pcmanfm nitrogen engrampa ranger cmus)
+	miku_logo
+	echo -e "\e[1;32m Instalando Dependencias..."
+	sleep 2
+	dependencias=(dunst rofi cmatrix polybar lxappearance pcmanfm nitrogen engrampa ranger cmus arandr)
 
-	printf "%s%sChecking for required packages...%s\n" "${BLD}" "${CBL}" "${CNC}"
+	echo -e "\e[1;32m Comprobando los paquetes nacesarios" 
 	for paquete in "${dependencias[@]}"
 	do
 	  if ! is_installed $paquete; then
 	    sudo pacman -S $paquete --noconfirm
 	    printf "\n"
 	  else
-	    printf '%s%s is already installed on your system!%s\n' "${CGR}" "$paquete" "${CNC}"
+	    echo -e "\e[1;32m $paquete ya está instalado en su sistema"
 	    sleep 1
 	  fi
 	done
-	sleep 2
+	
+	sleep 5
 	clear
 
-	miku_logo "Copying Dotfiles.."
-	printf "Copying files to respective directories..\n"
-
+	miku_logo
+	echo -e "\e[1;32m copiando dotfiles.."
 	if [ ! -d $HOME/.config ]; then
 		mkdir -p $HOME/.config
 		cp -r config/* $HOME/.config/
@@ -92,57 +84,60 @@ function install_dotfiles(){
 	else
 		cp -r misc/fonts/* $HOME/.local/share/fonts/
 	fi
-
-	printf "%s%sFiles copied succesfully!!%s\n" "${BLD}" "${CGR}" "${CNC}"
-	sleep 2
+	sleep 5
 	clear
 
-	miku_logo "Reloading fonts.."
-	fc-cache -rv >/dev/null 2>&1
-	printf "%s%sFonts reloaded succesfully!%s\n" "${BLD}" "${CGR}" "${CNC}"
-	sleep 2
-	clear
-
-	miku_logo "Installation finished"
-	printf "%s%sNow logout your session, select bspwm and log in.%s\n\n"
+	miku_logo
+	echo -en "\e[1;32m dotfiles copiados con exito\n"
+	echo -n -e "\e[1;37m"
 }
 
 function install_papirus_icon(){
 	wget -qO- https://git.io/papirus-icon-theme-install | sh
+	
+}
+
+function install_aplications(){
+	echo "hello world"
 }
 
 
 function title() {
-	echo -e "\e[1;32m        ____           __        ____ "
-	echo -e "       /  _/___  _____/ /_____ _/ / / "
-	echo -e "       / // __ \/ ___/ __/ __  / / /  "
-	echo -e "     _/ // / / (__  ) /_/ /_/ / / /  "
-	echo -e "    /___/_/ /_/____/\__/\__,_/_/_/   "
-	echo -e "                            /_/                      "
-	echo -e "\e[1;35m                                     By k3gc  "
+	echo -e "\e[1;32m
+	   ____         __       ____
+	  /  _/__  ___ / /____ _/ / /
+	 _/ // _ \(_-</ __/ _  / / /
+	/___/_//_/___/\__/\_,_/_/_/
+	"
+	echo -e "\e[1;35m				By k3gc"
 	echo " "
 }
 
+clear
 title
-echo -e "\e[1;36m  select an option:"
+echo -e "\e[1;36m  selecciona una opción:"
 echo " "
-echo -e "\e[1;36m  {1} -> \e[1;37mInstall dotfiles"
-echo -e "\e[1;36m  {2} -> \e[1;37mInstall papirus-icon-theme"
-echo -e "\e[1;36m  {3} -> \e[1;37mExit"
+echo -e "\e[1;36m  {1} -> \e[1;37mInstalar Miku-theme"
+echo -e "\e[1;36m  {2} -> \e[1;37mInstalar papirus-icon-theme"
+echo -e "\e[1;36m  {3} -> \e[1;37mInstalar aplicaciones"
+echo -e "\e[1;36m  {4} -> \e[1;37mSalir"
 echo " "
 echo -n -e "\e[1;36m  > "
 echo -n -e "\e[1;37m"
-read DD
+read option
 
 clear
 
-if [ $DD == 1 ]; then
+if [ $option == 1 ]; then
 	install_dotfiles
 fi
-if [ $DD == 2 ]; then
+if [ $option == 2 ]; then
 	install_papirus_icon
 fi
-if [ $DD == 3 ]; then
+if [ $option == 3 ]; then
+	install_aplications
+fi
+if [ $option == 4 ]; then
 	exit
 fi
 
