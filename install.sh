0d265596598ddfb4c23c7c1025c7196ae1941998
m!s4ki <119272540+k3gc@users.noreply.github.com>
2024-08-08 18:56:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,58 +1,300 @@
-#!/usr/bin/sh
+#!/bin/bash
 
-#export RICETHEME=$(<$HOME/.config/bspwm/rice.cfg)
+function title() {
+	echo -e "\e[1;32m
+	   ____         __       ____
+	  /  _/__  ___ / /____ _/ / /
+	 _/ // _ \(_-</ __/ _  / / /
+	/___/_//_/___/\__/\_,_/_/_/
+	"
+	echo -e "\e[1;35m				By k3gc"
+	echo " "
+}
 
-pkill sxhkd
-sxhkd -c $HOME/.config/bspwm/sxhkdrc &
+miku_logo () {
+		echo -en "\e[1;36m
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣶⣶⣶⣶⣶⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⢏⣼⠿⣦⠀⣠⣶⣿⣿⡿⣟⣿⣯⣿⢯⣿⣯⣿⢿⣿⣿⣦⣄⣀⣾⢿⣎⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⢣⡿⣛⣷⣿⣿⣿⢿⣯⣷⣿⡿⣯⣷⣿⢿⣯⣷⡿⣿⣻⣾⢿⡿⣿⣷⣿⣙⣧⡻⣦⠀⠀⠀⠀⢀⠀⠀⠀⠀
+	⠀⠀⢀⣤⠀⠀⠀⣰⡟⣡⣿⣿⡿⣟⣯⣿⣾⣿⣻⣽⣾⣿⢿⣽⣾⣿⣻⣽⣿⣟⣿⣽⡿⣿⣻⣷⡿⣿⣿⣷⡘⢷⡀⠀⣀⣾⣄⠀⠀⠀
+	⠀⢤⣾⣿⣷⠤⠈⣻⣆⢻⣿⣯⣿⢿⣻⣽⣾⣟⣿⣯⣷⡿⣿⠋⠓⠻⠿⠿⣾⣟⣯⣿⣟⣿⣟⣷⣿⢿⣾⣟⠏⣼⣏⠀⠙⣿⠋⡉⠀⠀
+	⠀⠀⠈⡿⠁⢠⣾⣿⣿⣦⢿⣯⣿⣿⣿⣿⣽⣾⡿⣽⣷⣿⣿⣷⣄⠀⠀⠀⠀⠉⢿⣷⣿⣿⣾⣿⣯⣿⣿⢎⣾⣿⣿⣷⣄⠸⣴⣧⡄⠀
+	⠀⠀⠀⠃⣠⣿⣿⣟⣿⢿⣧⣿⡿⣿⣿⣟⣿⠏⠀⠈⠛⠿⣿⣿⢿⣧⣀⠀⠀⠀⠀⠉⢿⣿⣯⣿⢿⣻⣽⣿⣿⣻⣽⣿⣿⣄⠈⠇⠀⠀
+	⠀⠀⠀⣰⣿⢿⣯⣿⣟⣿⣯⣿⣟⣿⡷⣿⠃⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠒⠂⠀⠀⠀⠹⣿⣽⡿⣿⣯⣿⣾⣟⣿⣾⡿⣿⣆⠀⠀⠀
+	⠀⠀⣴⣿⢯⣿⣟⣷⣿⣯⣷⣿⣻⣽⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣟⣿⡷⣿⣳⣿⣯⣷⣿⡽⣿⣆⠀⠀
+	⠀⢰⣿⡏⣾⣿⢯⣿⣾⢿⢸⣟⣿⣧⢿⡇⠀⢠⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⣤⣄⠀⠀⣿⣹⣿⣽⡟⣿⣷⡿⣯⣿⣷⢹⣿⡆⠀
+	⠀⣾⣿⢰⣿⡿⣿⣯⣿⡇⠸⣿⣿⢾⡎⠣⠀⠈⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠋⠀⠀⣡⡿⣯⣿⡇⢸⣿⣟⣿⡷⣿⡆⣿⣷⠀
+	⢰⣿⡇⣸⣿⣟⣿⡷⣿⡏⠀⢿⣿⠈⠻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠟⠁⣿⣿⠁⢸⣿⡿⣽⣿⢿⣇⢸⣿⡆
+	⢸⣿⡇⣿⣿⣾⢿⣽⣿⡇⠀⠸⣿⠀⠀⠈⠣⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⣿⠇⠀⢸⣿⢿⣿⣽⡿⣿⢸⣿⡇
+	⢸⣿⡇⣿⣷⡿⣿⣯⣷⡇⠀⠀⠘⡄⠀⠀⠀⠀⠑⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠊⠁⠀⠀⠀⠀⠏⠀⠀⢸⣿⣿⣻⣾⢿⣿⢸⣿⡇
+	⢸⣿⡇⢹⣿⣟⣿⣽⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⣽⣿⣻⡟⢸⣿⡇
+	⠀⣿⣷⢸⣿⣻⣯⣿⢿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⡿⣿⣽⡿⡇⣾⣿⠀
+	⠀⢹⣿⡆⣿⣿⣽⣿⣻⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⢿⣿⣽⣿⢰⣿⡏⠀
+	⠀⠀⢿⣿⡼⣿⣾⢿⣽⣟⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⡿⣟⣿⣾⢧⣿⡿⠀⠀
+	⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀
+	⠀⠀⠀⠈⢿⣿⢯⣷⣿⣯⣿⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⢾⣯⣿⢿⡽⠃⠀⠀⠀
+	⠀⠀⠀⠀⠀⢿⣿⣟⣷⡿⣯⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⢾⣿⣻⣽⠟⠁⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠈⢻⣯⣿⣟⣯⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣟⣾⣿⣻⡟⠁⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠘⠻⣿⣻⣷⣯⠛⠷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠾⠛⣽⣿⣯⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢽⣷⣄⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⣠⣾⡿⠗⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠓⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                            By sofy_32\n\n"
+		sleep 2
+}
 
-xsetroot -cursor_name left_ptr &
-picom --config ~/.config/bspwm/picom.conf &
-dunst -conf ~/.config/dunst/dunstrc &
 
-#$HOME/.fehbg &
-nitrogen --restore &
-$HOME/.config/bspwm/miku/polybar/launch.sh &
-$HOME/.config/bspwm/scripts/low_bat_notifier.sh &
-$HOME/.screenlayout/screen.sh &
+function install_dotfiles(){
 
-nm-applet --no-agent &
-setxkbmap -option grp:alt_shift_toggle us
+	distro="fedora"
+
+	clear
+	title
+	echo -e "\e[1;36m  selecciona tu distribucion:"
+	echo " "
+	echo -e "\e[1;36m  {1} -> \e[1;37mDebian"
+	echo -e "\e[1;36m  {2} -> \e[1;37mFedora"
+	echo -e "\e[1;36m  {3} -> \e[1;37mArch"
+	echo -e "\e[1;36m  {4} -> \e[1;37mSalir"
+	echo " "
+	echo -n -e "\e[1;36m  > "
+	echo -n -e "\e[1;37m"
+	read option
+
+	if [ $option == 1 ]; then
+		distro="debian"
+	fi
+	if [ $option == 2 ]; then
+		distro="fedora"
+	fi
+	if [ $option == 3 ]; then
+		distro="arch"
+	fi
+	if [ $option == 4 ]; then
+		exit
+	fi
+	
+	clear
+	miku_logo
+	echo -e "\e[1;32m Bienvenid@" $USER
+	echo -e "\e[1;32m Este script instalará las dependencias necesarias y copiará mis dotfiles a su configuración de bspwm."
+	sleep 5
+
+	while true; do
+		read -rp " Desea continuar? [y/N]: " yn
+			case $yn in
+				[Yy]* ) break;;
+				[Nn]* ) exit;;
+				* ) printf " Error: solo escribe 'y' o 'n'\n\n";;
+			esac
+    	done
+				clear
+
+	miku_logo
+	echo -e "\e[1;32m Instalando Dependencias..."
+	sleep 2
+	#dependencias=(polybar dunst rofi engrampa zsh cmatrix pcmanfm lxappearance nitrogen ranger cmus arandr light i3lock blueman xsetroot feh htop git pavucontrol hexyl)
+	dependencias=(kitty picom bspwm sxhkd polybar dunst rofi engrampa zsh cmatrix pcmanfm lxappearance nitrogen ranger cmus arandr light i3lock blueman xsetroot feh htop git pavucontrol hexyl zsh-syntax-highlighting zsh-autosuggestions)
+
+	if [ $distro == "debian" ]; then
+		is_installed() {
+    		dpkg-query -W -f='${Status}' $1 >/dev/null 2>&1
+    		return $?
+		}
+	fi
+
+	if [ $distro == "fedora" ]; then
+		is_installed() {
+	    	dnf list installed $1 >/dev/null 2>&1
+        	return $?
+		}
+	fi
+
+	if [ $distro == "arch" ]; then
+		is_installed() {
+	    	pacman -Qi $1 &> /dev/null
+        	return $?
+		}
+	fi
+
+	echo -e "\e[1;32m Comprobando los paquetes nacesarios" 
+	for paquete in "${dependencias[@]}"
+	do
+	  if ! is_installed $paquete; then
+
+	  	if [ $distro == "debian" ]; then
+			sudo apt install $paquete -y
+		fi
+		if [ $distro == "fedora" ]; then
+			sudo dnf install $paquete -y
+		fi
+		if [ $distro == "arch" ]; then
+			sudo pacman -S $paquete --noconfirm
+		fi
+
+	    printf "\n"
+	  else
+	    echo -e "\e[1;32m $paquete ya está instalado en su sistema"
+	    sleep 1
+	  fi
+	done
+	
+	sleep 5
+	clear
+
+	miku_logo
+	echo -e "\e[1;32m Asignando permisos de ejecucion"
+
+	chmod +x config/bspwm/bspwmrc
+	chmod +x config/bspwm/scripts/*.sh
+	chmod +x config/dunst/dunstrc
+	chmod +x config/miku/polybar/launch.sh
+	chmod +x config/ranger/scope.sh
+	chmod +x config/rofi/scripts/*.sh
+	chmod +x config/sxhkd/sxhkdrc
+	chmod +x home/.*
+
+	sleep 5
+	clear
+
+    miku_logo
+    echo -e "\e[1;32m Creando una copia de tu configuracion.."
+
+    if [ ! -d $HOME/.ThemeBakup ]; then
+		mkdir -p $HOME/.ThemeBakup
+	fi
+
+    [ -d ~/.config/bspwm ] && mv ~/.config/bspwm ~/.ThemeBakup/bspwm-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/kitty ] && mv ~/.config/kitty ~/.ThemeBakup/kitty-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/nitrogen ] && mv ~/.config/nitrogen ~/.ThemeBakup/nitrogen-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/cmus ] && mv ~/.config/cmus ~/.ThemeBakup/cmus-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/neofetch ] && mv ~/.config/neofetch ~/.ThemeBakup/neofetch-backup-"$(date +%Y.%m.%d-%H.%M.%S)"   
+    [ -d ~/.config/picom ] && mv ~/.config/picom ~/.ThemeBakup/picom-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/rofi ] && mv ~/.config/rofi ~/.ThemeBakup/rofi-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/sxhkd ] && mv ~/.config/sxhkd ~/.ThemeBakup/sxhkd-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/dunst ] && mv ~/.config/dunst ~/.ThemeBakup/dunst-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/polybar ] && mv ~/.config/polybar ~/.ThemeBakup/polybar-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/ranger ] && mv ~/.config/ranger ~/.ThemeBakup/ranger-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -d ~/.config/zsh ] && mv ~/.config/zsh ~/.ThemeBakup/zsh-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    [ -f ~/.zshrc ] && mv ~/.zshrc ~/.ThemeBakup/.zshrc-backup-"$(date +%Y.%m.%d-%H.%M.%S)"
+    
+    echo -e "\e[1;32m listo!"
+    sleep 5
+	clear
+
+	miku_logo
+	echo -e "\e[1;32m copiando dotfiles.."
 
 
-SECOND_MONITOR_SIDE="left"
-THIRD_MONITOR_SIDE="rigth"
+	if [ ! -d $HOME/.config ]; then
+		mkdir -p $HOME/.config
+		cp -r config/* $HOME/.config/
+	else
+		cp -r config/* $HOME/.config/
+	fi
 
-Get Monitors
-PRINCIPAL=$(xrandr | grep primary | awk '{print $1}')
-SECOND_MONITOR=$(xrandr | grep connected | awk '{i++}i==2'| awk '{print $1}')
-THIRD_MONITOR=$(xrandr | grep connected | awk '{i++}i==3'| awk '{print $1}')
 
-Configure Monitors
-if [ "$(xrandr | grep -sw 'connected' | wc -l)" = "2" ]; then
-  bspc monitor $(bspc query -M | sed -n 1p) -d I II III IV V VI VII
-  bspc monitor $(bspc query -M | sed -n 2p) -d VIII IX X
-  #Set Monitors Side and output 
-  xrandr --output $SECOND_MONITOR --$SECOND_MONITOR_SIDE-of $PRINCIPAL  
-else
-  bspc monitor -d I II III IV V VI VII
+	if [ ! -d $HOME/.local/share/fonts ]; then
+		mkdir -p $HOME/.local/share/fonts
+		cp -r misc/fonts/* $HOME/.local/share/fonts/
+		cp -r home/.* $HOME/
+	else
+		cp -r misc/fonts/* $HOME/.local/share/fonts/
+		cp -r home/.* $HOME/
+	fi
+
+	if [ ! -d $HOME/.themes ]; then
+		mkdir -p $HOME/.themes
+		#cp -r misc/themes/* $HOME/.themes/
+		tar -xf misc/themes/CyberHack-master.tar.gz -C $HOME/.themes/
+	else
+		#cp -r misc/themes/* $HOME/.themes/
+		tar -xf misc/themes/CyberHack-master.tar.gz -C $HOME/.themes/
+	fi
+
+	if [ ! -d $HOME/.icons ]; then
+		mkdir -p $HOME/.icons
+		#cp -r misc/cursor/* $HOME/.icons/
+		tar -xf misc/cursor/Sanae.tar.gz -C $HOME/.icons/
+	else
+		#cp -r misc/cursor/* $HOME/.icons/
+		tar -xf misc/cursor/Sanae.tar.gz -C $HOME/.icons/
+	fi
+
+	sleep 5
+	clear
+	
+	#chsh -s /usr/bin/zsh
+
+	miku_logo
+	echo -en "\e[1;32m dotfiles copiados con exito\n"
+	echo -n -e "\e[1;37m"
+
+	sleep 5
+	clear
+
+	miku_logo
+	echo -e "\e[1;32m Instalando ohmyzsh"
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+}
+
+function install_papirus_icon(){
+	wget -qO- https://git.io/papirus-icon-theme-install | sh
+	
+}
+
+function install_aplications(){
+
+	applications=(gimp krita deadbeef simplescreenrecorder shotcut vlc qimgv)
+
+	is_installed() {
+	    dnf list installed $1 >/dev/null 2>&1 ;
+        return $?
+	}
+
+	echo -e "\e[1;32m Comprobando los paquetes nacesarios" 
+	for paquete in "${applications[@]}"
+	do
+	  if ! is_installed $paquete; then
+	    sudo dnf install $paquete -y
+	    printf "\n"
+	  else
+	    echo -e "\e[1;32m $paquete ya está instalado en su sistema"
+	    sleep 1
+	  fi
+	done
+}
+
+
+clear
+title
+echo -e "\e[1;36m  selecciona una opción:"
+echo " "
+echo -e "\e[1;36m  {1} -> \e[1;37mInstalar rice-miku"
+echo -e "\e[1;36m  {2} -> \e[1;37mInstalar papirus-icon-theme"
+echo -e "\e[1;36m  {3} -> \e[1;37mInstalar aplicaciones"
+echo -e "\e[1;36m  {4} -> \e[1;37mInstalar pico-sdk"
+echo -e "\e[1;36m  {5} -> \e[1;37mSalir"
+echo " "
+echo -n -e "\e[1;36m  > "
+echo -n -e "\e[1;37m"
+read option
+
+clear
+
+
+if [ $option == 1 ]; then
+	install_dotfiles
 fi
-
-#bspc monitor -d I II III IV V VI VII
-#bspc monitor HDMI-A-0 -d VIII IX X
-
-
-bspc config border_width         3
-bspc config focused_border_color \#02db95 
-bspc config normal_border_color \#6ce2e2
-bspc config window_gap          12
-
-bspc config split_ratio          0.52
-bspc config borderless_monocle   true
-bspc config gapless_monocle      true
-
-bspc rule -a Gimp desktop='^2' state=tiled follow=on
-bspc rule -a firefox desktop='^4' state=tiled follow=on
-bspc rule -a Chromium desktop='^2'
-bspc rule -a mplayer2 state=floating
-bspc rule -a Kupfer.py focus=on
-bspc rule -a Screenkey manage=off
+if [ $option == 2 ]; then
+	install_papirus_icon
+fi
+if [ $option == 3 ]; then
+	install_aplications
+fi
+if [ $option == 4 ]; then
+	install_pico_sdk
+fi
+if [ $option == 5 ]; then
+	exit
+fi
